f9f60731cf735b3215fce35405973dd40a002b60
StayCake <36215887+StayCake@users.noreply.github.com>
2022-02-18 13:51:11 +0900

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import configparser
-
-config = configparser.ConfigParser()
-config.read('main.ini')
-
-token = config['DATA']['token'].strip()
-prefix = config['DATA']['prefix'].strip()
-ownerid = config['DATA']['ownerid'].strip()
